c4a043f4e6812096913bb33364877f2dd16c25e8
Mllyszn <melvinlieuw121107@gmail.com>
2024-06-17 12:09:11 +0200

    maps

--- core/overmijpage.php
+++ core/overmijpage.php
@@ -6,7 +6,7 @@ include ("../database/db_connect.php");
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contactpagina</title>
+    <title>Over mij</title>
     <link rel="icon" href="<?= BASEURL ?>/assets/img/weblogo.ico">
     <link rel="stylesheet" href="<?= BASEURL ?>assets/css/overmijStyle.css">
     <link rel="stylesheet" href="<?= BASEURL ?>assets/css/header.css">
@@ -17,7 +17,9 @@ include ("../database/db_connect.php");
     <div id="container">
         <?php include (BASE_PATH . "/header.php"); ?>
         <main>
-        
+            <div class="selfie">
+                <img src="/assets/img/Hendrik_foto.png" alt="Selfie Hendrik">
+            </div>
         </main>
         <?php include (BASE_PATH . "/footer.php"); ?>
     </div>
