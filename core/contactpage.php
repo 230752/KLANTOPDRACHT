b4c0a76b7bbf2a8834415bc4e93695117fd078c4
Mllyszn <melvinlieuw121107@gmail.com>
2024-06-19 14:52:15 +0200

    responsive

--- core/contactpage.php
+++ core/contactpage.php
@@ -14,42 +14,43 @@ include ("../database/db_connect.php");
 </head>
 
 <body>
-    <div id="container">
-        <?php include (BASE_PATH . "/header.php"); ?>
-        <main>
-            <h1>Contact Formulier</h1>
-            <div class="contactform">
-                <form action="overmijpage.php" method="post">
+    <?php include (BASE_PATH . "/header.php"); ?>
 
-                    <label for="vnaam">Voornaam</label>
-                    <input type="text" id="vnaam" name="vnaam" placeholder="Uw voornaam.." required>
+    <main>
+        <div class="fpage">
+            <div>
+                <H1>Contact Formulier</H1>
+                <div class="contactform">
+                    <form action="overmijpage.php" method="post">
 
-                    <label for="anaam">Achternaam</label>
-                    <input type="text" id="anaam" name="anaam" placeholder="Uw achternaam.." required>
+                        <label for="vnaam">Voornaam</label>
+                        <input type="text" id="vnaam" name="vnaam" placeholder="Uw voornaam.." required>
 
-                    <label for="adres">Adres</label>
-                    <input type="text" id="adres" name="adres" placeholder="Uw adres.." required>
+                        <label for="anaam">Achternaam</label>
+                        <input type="text" id="anaam" name="anaam" placeholder="Uw achternaam.." required>
 
-                    <label for="telnummer">Telefoon nummer</label>
-                    <input type="text" id="telnummer" name="telnum" placeholder="Uw telefoon nummer.." required>
+                        <label for="adres">Adres</label>
+                        <input type="text" id="adres" name="adres" placeholder="Uw adres.." required>
 
-                    <label for="email">E-mail</label>
-                    <input type="text" id="email" name="email" placeholder="Uw e-mail.." required>
+                        <label for="telnummer">Telefoon nummer</label>
+                        <input type="text" id="telnummer" name="telnum" placeholder="Uw telefoon nummer.." required>
 
-                    <label for="subject">Vraag/Opmerking</label>
-                    <textarea id="subject" name="opmrk" placeholder="Schrijf hier.." rows="4" cols="50"></textarea>
+                        <label for="email">E-mail</label>
+                        <input type="text" id="email" name="email" placeholder="Uw e-mail.." required>
 
-                    <input type="submit" value="Maak afspraak" name="add">
-                </form>
+                        <label for="subject">Vraag/Opmerking</label>
+                        <textarea id="subject" name="opmrk" placeholder="Schrijf hier.." rows="4" cols="50"></textarea>
+
+                        <input type="submit" value="Maak afspraak" name="add">
+                    </form>
+                </div>
             </div>
-            <div class="info">
-                <div>
+            <div class="right-info">
+                <div class="info">
                     <iframe id="google-maps"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4899.554601795917!2d5.266849776508817!3d52.120180671959154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c642613b5a8d0b%3A0x25dde48235d95baf!2sBeukbergenhof%2093%2C%203712%20BX%20Huis%20Ter%20Heide!5e0!3m2!1snl!2snl!4v1718619485389!5m2!1snl!2snl"
                         allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
                     </iframe>
-                </div>
-                <div>
                     <H4>
                         Maandag - Vrijdag: 07.00 - 17.00 uur
                         <br>
@@ -57,22 +58,22 @@ include ("../database/db_connect.php");
                         <br>
                         Zondag: Gesloten
                     </H4>
-                    <br>
                     <H5>
-                        We streven ernaar om constant in contact te staan met onze klanten totdat de klus geklaard is.
+                        We streven ernaar om constant in contact te staan met onze klanten totdat de klus geklaard
+                        is.
                         Als u
                         vragen
-                        of speciale verzoeken heeft, stuur ons dan een bericht. Voor een vrijblijvende offerte kunt u
+                        of speciale verzoeken heeft, stuur ons dan een bericht. Voor een vrijblijvende offerte kunt
+                        u
                         contact
                         met
                         ons opnemen wanneer het u uitkomt. Wij zijn u graag van dienst!
                     </H5>
                 </div>
             </div>
-    </div>
+        </div>
     </main>
     <?php include (BASE_PATH . "/footer.php"); ?>
-    </div>
 </body>
 
 </html>
